25825045cef04dcb89816787031d287caca8013f
Mladen Dubovac <mladendubovac@live.com>
2021-09-15 23:35:43 +0200

    Replace carousel package and further optimize code

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,72 +1,81 @@
-import React from 'react';
-import Carousel from 'react-multi-carousel';
+import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModalState } from '../../hooks/useModalState';
 import { addToCart } from '../../store/actions/cart/cart_action';
 import { formatPrice } from '../../utils/helper_function';
 import './Carousel.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
-const responsive = {
-    desktop: {
-        breakpoint: { max: 3000, min: 1280 },
-        items: 2
-    },
-    tablet: {
-        breakpoint: { max: 1280, min: 768 },
-        items: 2
-    },
-    mobile: {
-        breakpoint: { max: 768, min: 0 },
-        items: 1
-    },
+const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    centerMode: true,
+    centerPadding: "100px",
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                centerPadding: "0"
+            }
+        }
+    ]
 };
 
 const ProductsCarousel = ({ products }) => {
     const { toggleCart } = useModalState();
     const dispatch = useDispatch();
+    const ref = useRef(null);
 
      const handleClickAddToCard = (product) => {
         dispatch(addToCart(product));
         toggleCart();
     }
 
+    const handleNavigation = (direction) => {
+        const carousel = ref.current;
+        direction === 'next' ? carousel.slickNext() : carousel.slickPrev();
+    }
+
     return (
-			<Carousel
-				swipeable={true}
-				draggable={true}
-				responsive={responsive}
-				ssr={true}
-				keyBoardControl={true}
-				customTransition="all .5"
-				transitionDuration={750}
-				containerClass="carousel-container"
-				removeArrowOnDeviceType={["tablet", "mobile"]}
-				itemClass="carousel-item-padding-40-px"
-			>
-				{products.length > 0 &&
-					products.map((product, index) => (
-						<div className="product-wrapper" key={index}>
-							<img
-								className="product-image"
-								src={product.image}
-								alt={product.title}
-							/>
-							<h3 className="product-title">{product.title}</h3>
-							<p className="product-description">
-								This is a short product description. You can find more details <abbr title={product.description}>here</abbr>.
-								{/* { product.description } */}
-							</p>
-							<p className="product-price">{formatPrice(product.price)}</p>
-							<button
-								className="product-add-to-cart"
-								onClick={() => handleClickAddToCard(product)}
-							>
-								Add to cart
-							</button>
-						</div>
-					))}
-			</Carousel>
-		);
+        products.length > 0 &&
+            <>
+                <Slider {...settings} ref={ref}>
+                    {products.map((product, index) => (
+                        <div className="product-wrapper" key={index}>
+                            <img
+                                className="product-image"
+                                src={product.image}
+                                alt={product.title}
+                            />
+                            <h3 className="product-title">{product.title}</h3>
+                            <p className="product-description">
+                                This is a short product description. You can find more details <abbr title={product.description}>here</abbr>.
+                                {/* { product.description } */}
+                            </p>
+                            <p className="product-price">{formatPrice(product.price)}</p>
+                            <button
+                                className="product-add-to-cart"
+                                onClick={() => handleClickAddToCard(product)}
+                            >
+                                Add to cart
+                            </button>
+                        </div>
+                    ))}
+                </Slider>
+                <div className="product-navigation">
+                    <FaArrowLeft className="product-navigation-arrow" onClick={() => handleNavigation('previous')} />
+                    <FaArrowRight className="product-navigation-arrow" onClick={() => handleNavigation('next')} />
+                </div>
+            </>
+    );
 }
 
 export default ProductsCarousel;
--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -2,19 +2,20 @@ import { useEffect, useState } from 'react';
 
 export const OutsideClick = (ref) => {
 	const [isClicked, setIsClicked] = useState();
+
 	useEffect(() => {
-		function handleClickOutside(event) {
-			if (ref.current && !ref.current.contains(event.target)) {
-				setIsClicked(true);
-			} else {
-				setIsClicked(false);
-			}
-		}
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setIsClicked(true);
+            }
+            return setIsClicked(false);
+        }
 
 		document.addEventListener('mousedown', handleClickOutside);
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
 	}, [ref]);
+
 	return isClicked;
 };
